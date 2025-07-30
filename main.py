61a94cc995aed087ca3fff89c2797fcaf0fc3b01
nikhathmuzawar <nikhath.fatimam@gmail.com>
2025-07-30 15:44:13 +0530

    cleaner

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
-import cv2
-import av
-import asyncio
+from aiortc import RTCPeerConnection, RTCSessionDescription
+from aiortc.contrib.media import MediaPlayer
+import uvicorn
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 RTSP_URL = "rtsp://localhost:8554/mystream"
 
-
 @app.get("/", response_class=HTMLResponse)
 async def index():
     with open("static/index.html") as f:
         return f.read()
 
-from aiortc.contrib.media import MediaPlayer
-
 @app.post("/offer")
 async def offer(request: Request):
     params = await request.json()
     offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
-
     pc = RTCPeerConnection()
-
     player = MediaPlayer(RTSP_URL, format="rtsp", options={
         "rtsp_transport": "tcp",
         "fflags": "nobuffer",
@@ -34,19 +28,15 @@ async def offer(request: Request):
         "framedrop": "1",
         "max_delay": "500000"
     })
-
     if player.video:
         pc.addTrack(player.video)
-
     await pc.setRemoteDescription(offer)
     answer = await pc.createAnswer()
     await pc.setLocalDescription(answer)
-
     return {
         "sdp": pc.localDescription.sdp,
         "type": pc.localDescription.type
     }
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host='0.0.0.0', port=8000)
