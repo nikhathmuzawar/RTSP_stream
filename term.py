b6d38b815ef2bf4de3e9b5873a8657cfe4ba9232
nikhathmuzawar <nikhath.fatimam@gmail.com>
2025-08-06 17:29:28 +0530

    added shell to webpage

--- term.py
+++ term.py
@@ -1,18 +1,19 @@
-from fastapi import FastAPI, Request,  WebSocket, WebSocketDisconnect
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from aiortc import RTCPeerConnection, RTCSessionDescription
-from aiortc.contrib.media import MediaPlayer
-import uvicorn
 import asyncio
 import os
 import pty
-import threading
 import select
+import subprocess
+import threading
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def get_index():
+    return FileResponse("term.html")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
@@ -21,38 +22,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-RTSP_URL = "rtsp://localhost:8554/mystream"
-
-@app.get("/", response_class=HTMLResponse)
-async def index():
-    with open("static/index.html") as f:
-        return f.read()
-
-@app.post("/offer")
-async def offer(request: Request):
-    params = await request.json()
-    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
-    pc = RTCPeerConnection()
-    player = MediaPlayer(RTSP_URL, format="rtsp", options={
-        "rtsp_transport": "tcp",
-        "fflags": "nobuffer",
-        "flags": "low_delay",
-        "threads": "1",
-        "framedrop": "1",
-        "max_delay": "500000"
-    })
-    if player.video:
-        pc.addTrack(player.video)
-    await pc.setRemoteDescription(offer)
-    answer = await pc.createAnswer()
-    await pc.setLocalDescription(answer)
-    return {
-        "sdp": pc.localDescription.sdp,
-        "type": pc.localDescription.type
-    }
-
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    print("WebSocket request headers:", websocket.headers)
     await websocket.accept()
     loop = asyncio.get_event_loop()
     pid, master_fd = pty.fork()
@@ -109,7 +81,3 @@ async def websocket_endpoint(websocket: WebSocket):
                 os.close(master_fd)
             except Exception:
                 pass
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000)
